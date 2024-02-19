5a506ef5146c022b5a0626df607968baa0b0d073
Jarek Grygolec <jgrygolec@gmail.com>
2024-02-19 11:11:17 +0100

    added github repo creation

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+github_username="{{ cookiecutter.github_username }}"
+
+if [[ ! -z $github_username ]]; then
+    cd {{ cookiecutter.project_path }}
+    gh repo create {{ cookiecutter.project_slug }} --private
+    git init
+    git add -A
+    git commit -m "initializing the repository"
+    git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git
+    git push -u -f origin main
+    cd $was_path
+fi
