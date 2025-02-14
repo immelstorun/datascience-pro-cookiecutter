e11fb71a2c0b924352fdd06fe1e6c50eedab7d69
Сейкин Алексей <aseikin@cspfmba.ru>
2025-02-14 12:14:40 +0300

    remove LR

--- {{ cookiecutter.project_slug }}/.devcontainer/post-install.sh
+++ {{ cookiecutter.project_slug }}/.devcontainer/post-install.sh
@@ -1,5 +1,5 @@
-# Enabling .bashrc for the project
-echo -e "\n# sourcing project .projectrc\nif [ -f \"\$PROJECT_PATH/.projectrc\" ]; then\n    . \"\$PROJECT_PATH/.projectrc\"\nfi" >> /home/vscode/.bashrc
+#  Enabling .bashrc for the project
+echo -e "\n# sourcing project .projectrc\nif [ -f \$PROJECT_PATH/.projectrc ]; then\n    . \$PROJECT_PATH/.projectrc\nfi" >> /home/vscode/.bashrc
 
 # Configuring the poetry environment
 if [ -f "${PROJECT_PATH}/pyproject.toml" ]; then
