1ba38cfdcaa38613c5f672ebe00f4e0fc47ba833
Сейкин Алексей <aseikin@cspfmba.ru>
2025-02-14 10:47:54 +0300

    Создаём ветку main вместо master

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,6 +5,7 @@ if [[ ! -z $github_username ]]; then
     cd {{ cookiecutter.project_path }}
     gh repo create {{ cookiecutter.project_slug }} --private
     git init
+    git checkout -b main  # Создаём ветку main вместо master
     git add -A
     git commit -m "initializing the repository"
     git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git
